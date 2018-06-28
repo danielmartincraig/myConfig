c562f71da96de8570fff838cee25782e28a112b2
Daniel Craig <dmcraig@ldschurch.org>
2018-06-28 09:10:05 -0600

    initial commit

---
+++ config.sh
@@ -0,0 +1,14 @@
+# Install Spacemacs
+sudo apt install emacs25
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+# Install Webstorm
+sudo snap install webstorm --classic
+# Install Intellij
+sudo snap install intellij-idea-ultimate --classic --edge
+
+# Create Repos Directory
+mkdir ~/Repos
+
+# Clone Repos
+git clone https://github.com/AlldredgeCraigSeniorProjectTeam/oral-history
